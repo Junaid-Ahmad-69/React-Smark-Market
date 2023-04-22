8c2da22ff7a5293dbbecd22760507b19d02f0fd3
Junaid Sandhu <junaidsandhu21@gmail.com>
2023-04-22 22:29:18 +0500

    Created & add API in .env file but give error 403

--- react-smart-market/src/utils/api.js
+++ react-smart-market/src/utils/api.js
@@ -1,18 +1,15 @@
 import axios from "axios";
 
+
 const params = {
     headers: {
-        Authorization: "bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
+        Authorization: "Bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
     }
 }
 
-const REACT_APP_DEV_KEY= "http://localhost:1337"
-
 export const fetchDataFromAPI = async (url) => {
     try {
-        const {data} = await axios.get(REACT_APP_DEV_KEY + url, {
-            Authorization: "bearer" + process.env.REACT_APP_STRIPE_APP_KEY
-        });
+        const data = await axios.get(process.env.REACT_APP_DEV_KEY + url, params);
         return data;
     } catch (err) {
         console.log(err)
